0a23f1ffe57d5842f2365ef09bca0cc0b18eee72
Naomi Mardon <naomimardon@Naomis-MBP.broadband>
2023-02-27 15:17:41 +0000

    Created README file using one prompt

--- index.js
+++ index.js
@@ -5,17 +5,29 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of the README file?'
+    }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-}
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log('Success!')
+    );
+};
 
 // function to initialize program
 function init() {
-
-}
+    inquirer
+    .prompt(questions)
+    .then((data) => {
+        const filename = `README.md`;
+        writeToFile(filename, generateMarkdown(data))
+    });
+};
 
 // function call to initialize program
 init();
