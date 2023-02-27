9a40e312d5edbb85362f448f5f989ef3a25ca2cc
Naomi Mardon <naomimardon@Naomis-MBP.broadband>
2023-02-27 15:36:05 +0000

    updated generateMarkdown.js to create full structure of README file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,54 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+
+  ${data.description}
+
+  Here is a link to the deployed application:
+  ${data.appLink}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ![${data.altText}](${data.screenshot})
+
+  ## Credits
+
+  ${data.credits}
+
+  ## License
+
+  ${data.license}
+
+  ## Badges
+
+  ${data.license}
+
+  ## Features
+
+  ${data.features}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Test
+
+  ${data.tests}
+
 `;
 }
 
