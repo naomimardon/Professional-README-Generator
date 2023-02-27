a0fa58b4e253f5d505caeea45e9f2dda01cbdc49
Naomi Mardon <naomimardon@Naomis-MBP.broadband>
2023-02-27 16:30:14 +0000

    updated index.js to include all questions

--- index.js
+++ index.js
@@ -8,8 +8,64 @@ const questions = [
     {
         type: 'input',
         name: 'title',
-        message: 'What is the title of the README file?'
-    }
+        message: 'What is the title of the README file?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Give a description of the project, explaining what the application does, your motivation for building it, and what you learnt:',
+    },
+    {
+        type: 'input',
+        name: 'appLink',
+        message: 'Provide a link to the deployed application:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions on how to use your application:',
+    },
+    {
+        type: 'input',
+        name: 'screenshot',
+        message: 'Provide a link to a screenshot of the application:',
+    },
+    {
+        type: 'input',
+        name: 'altText',
+        message: 'Provide some alt text describing the screenshot:',
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators with links to their GitHub profiles:',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license did you use for your project? (Use the down arrow to choose one):',
+        choices: ['None','MIT License','GNU General Public License v3.0','Apache License 2.0', 'Other'],
+    },
+    {
+        type: 'input',
+        name: 'features',
+        message: 'List the features of your project:',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide guidelines for how to contribute to this project:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Write tests for your application and provide examples on how to run them:',
+    },
 ];
 
 // function to write README file
